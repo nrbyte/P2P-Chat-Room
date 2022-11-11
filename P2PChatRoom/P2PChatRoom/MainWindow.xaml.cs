17a24bf3fba2492b38bd90f15be2a82f5f0dfe1f
sami0505 <sami0505@abv.bg>
2022-11-11 20:10:39 +0000

    Added showMessage() Method and removed remnants of position alert
    showMessage() is used upon construction to show it's function.

--- P2PChatRoom/P2PChatRoom/MainWindow.xaml.cs
+++ P2PChatRoom/P2PChatRoom/MainWindow.xaml.cs
@@ -27,10 +27,17 @@ namespace P2PChatRoom
             // Constructor for MainWindow
             InitializeComponent();
 
-
             Uri icon = new Uri($"{path}\\images\\P2P.ico", UriKind.Absolute);
             ImageSource iconSource = new BitmapImage(icon);
             this.Icon = iconSource;
+
+            showMessage("Neel's Laptop", "IMPORTANT ANNOUCEMENT 25TH OCTOBER 1881\n---------------------------------------\n\nThe Imperial Empire of Japan has attached our precious naval islands with their navy.\n\nCongress has approved $100 million in military funding to defeat this upcoming enemy.");
+        }
+
+        public void showMessage(string deviceName, string messageReceived)
+        {
+            this.deviceName.Text = deviceName;
+            this.messageDisplayed.Text = messageReceived;
         }
     }
 }
