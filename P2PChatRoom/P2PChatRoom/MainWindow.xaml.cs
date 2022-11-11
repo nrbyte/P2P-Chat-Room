9ec627b5b01cd7ab16fe879382be6d90bfc71036
sami0505 <sami0505@abv.bg>
2022-11-11 20:48:39 +0000

    Added StateChanged taskbar removal
    It still doesn't show in system tray though.

--- P2PChatRoom/P2PChatRoom/MainWindow.xaml.cs
+++ P2PChatRoom/P2PChatRoom/MainWindow.xaml.cs
@@ -31,13 +31,21 @@ namespace P2PChatRoom
             ImageSource iconSource = new BitmapImage(icon);
             this.Icon = iconSource;
 
+            
+            // Placeholder
             showMessage("Neel's Laptop", "IMPORTANT ANNOUCEMENT 25TH OCTOBER 1881\n---------------------------------------\n\nThe Imperial Empire of Japan has attached our precious naval islands with their navy.\n\nCongress has approved $100 million in military funding to defeat this upcoming enemy.");
-        }
+        } 
 
         public void showMessage(string deviceName, string messageReceived)
         {
             this.deviceName.Text = deviceName;
             this.messageDisplayed.Text = messageReceived;
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Minimized) ShowInTaskbar = false;
+            else ShowInTaskbar = true;
         }
     }
 }
