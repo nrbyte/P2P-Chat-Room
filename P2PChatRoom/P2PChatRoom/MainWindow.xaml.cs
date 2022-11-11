c7b140c6d8016ca165447bbd6c1ca3e3291b06ed
sami0505 <sami0505@abv.bg>
2022-11-11 19:32:33 +0000

    Added icon and display code for icon
    The venerable icon has been added!

--- P2PChatRoom/P2PChatRoom/MainWindow.xaml.cs
+++ P2PChatRoom/P2PChatRoom/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace P2PChatRoom
 {
@@ -20,9 +21,13 @@ namespace P2PChatRoom
     /// </summary>
     public partial class MainWindow : Window
     {
+        string path = Directory.GetCurrentDirectory();
         public MainWindow()
         {
             InitializeComponent();
+            Uri icon = new Uri($"{path}\\images\\P2P.ico", UriKind.Absolute);
+            ImageSource iconSource = new BitmapImage(icon);
+            this.Icon = iconSource;
             MainGrid.MouseUp += new MouseButtonEventHandler(MainGrid_MouseUp);
             MainGrid.MouseDown += new MouseButtonEventHandler(MainGrid_MouseDown);
         }
