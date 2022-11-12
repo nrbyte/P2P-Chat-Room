3f3fcd24f24460c034e0cb63273b6483ab91ff65
sami0505 <sami0505@abv.bg>
2022-11-12 15:05:42 +0000

    NewConnection.xaml added
    Added NewConnection dialog window, which can take deviceName and IPAdress for new connection. Validation not yet added.

---
+++ P2PChatRoom/P2PChatRoom/NewConnection.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace P2PChatRoom
+{
+    /// <summary>
+    /// Interaction logic for NewConnection.xaml
+    /// </summary>
+    public partial class NewConnection : Window
+    {
+        public NewConnection()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDialogOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            deviceName.SelectAll();
+            deviceName.Focus();
+
+//            IPAddress.SelectAll();
+//            IPAddress.Focus();
+        }
+    }
+}
